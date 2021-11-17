bd67eb5929610f5d99c1898861a972d4b24435c1
julian776 <juliancamiloalvarez77@gmail.com>
2021-11-17 14:36:51 -0500

    El juego esta terminado y funcionando

--- app.js
+++ app.js
@@ -2,3 +2,33 @@ let numeroMinimo = 1;
 
 let numeroMaximo = 100;
 
+let numeroSecreto = Math.random() * numeroMaximo;
+numeroSecreto = parseInt(numeroSecreto);
+
+let intentos = 5;
+
+let numeroIngresado = prompt("Ingresa el número que creas es el número magico", "0");
+while(intentos > 0){
+    numeroIngresado = parseInt(numeroIngresado);
+
+    if(numeroIngresado === 0)
+        break;
+    else if(numeroIngresado === numeroSecreto){
+        alert("FELICITACIONES!!! GANASTE");
+        break;
+    }
+    else if (numeroIngresado > numeroSecreto){
+        numeroIngresado = prompt("El numero magico es menor, ingresa uno nuevo: ")
+        intentos --
+    }
+    else if (numeroIngresado < numeroSecreto){
+        numeroIngresado = prompt("El número magico es mayor, ingresa uno nuevo: ");
+        intentos --
+    }
+    
+}
+
+if(intentos == 0){
+    alert("Perdiste se acabaron los intentos! el numero era "+ numeroSecreto);
+}
+
